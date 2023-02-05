d7a421096a7bb3c848e6de26aa8ecc06889991e5
ANIRBARASKAR <105123608+ANIRBARASKAR@users.noreply.github.com>
2023-02-05 21:03:21 +0530

    photo-book fabric

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import MaskPath from './MaskPath';
+import MaskPathImages from './MaskPathImages';
+import PhotoboxMasking from './PhotoboxMasking';
+import PhotoBook from './photo_book/PhotoBook';
+
+
+
+const App = () => {
+	return (
+		<div>
+			{/* <PhotoboxMasking/> */}
+
+			{/* <MaskPath/> */}
+
+			  {/* <MaskPathImages/> */}
+
+			  {/* final photobook */}
+
+			  <PhotoBook/>
+		</div>
+	);
+}
+
+export default App;
